6383d99815b27d6fc985fae926abc52348089855
Avinash Limbu <50650789+Avinashlimbu@users.noreply.github.com>
2025-05-09 17:58:24 +0900

    Done

--- events/script.js
+++ events/script.js
@@ -1,13 +1,23 @@
-const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function(e){
-    e.target.style.background = "blue";
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+    })
 })
 
 
 
 
 /*
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function(e){
+    e.target.style.background = "blue";
+})
 
 //Method #2
 const btn = document.querySelector("#btn");
